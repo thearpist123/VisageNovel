fd1b01f5abb6d8225ba7f4066136b11eb50b63cb
Adam Heeren <thearpist123@gmail.com>
2020-08-19 22:31:44 -0400

    removing auth from web.php

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/projects', 'ProjectController');
