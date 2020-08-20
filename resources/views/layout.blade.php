26276595a7d21140873b99fdbb97553c6920a180
Adam Heeren <thearpist123@gmail.com>
2020-08-19 21:57:31 -0400

    removing unused css classes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -21,65 +21,10 @@
                 margin-bottom:3em;
                 margin-top:3em;
                 display:flex;
-            }        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
             }
         </style>
 </head>
 <body>
-  
-
-@if (Route::has('login'))
-<div class="top-right">
-    @auth
-        <a href="{{ url('/') }}">Home</a>
-    @else
-        <a href="{{ route('login') }}">Login</a>
-
-        @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-        @endif
-    @endauth
-</div>
-@endif
 <div class="container">
     <div class="links">
         <a href="/tasks">Task list</a>
